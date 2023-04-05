a3bf30b6021c21487239fc8a3307c9d56870b3c5
Daniel <daniel@sohe.ir>
2023-04-05 11:53:15 +0330

    chore(user): changes in user schema

--- src/domains/user/schemas/user.schema.ts
+++ src/domains/user/schemas/user.schema.ts
@@ -1,6 +1,8 @@
 import { Type, Static } from '@sinclair/typebox';
+import removeItemsIn from '$src/infra/removeItemsIn';
 
 export const UserSchema = Type.Object({
+  id: Type.Number(),
   firstName: Type.String(),
   lastName: Type.String(),
   role: Type.Object({ id: Type.Number() }),
@@ -10,6 +12,50 @@ export const UserSchema = Type.Object({
   password: Type.String(),
   position: Type.Optional(Type.String()),
   isActive: Type.Boolean(),
+  createdAt: Type.Union([Type.Date(), Type.String()]),
+  updatedAt: Type.Union([Type.Date(), Type.String()]),
+  deletedAt: Type.Union([Type.Date(), Type.String(), Type.Null()]),
 });
 
 export type UserType = Static<typeof UserSchema>;
+
+export const UserExample: UserType = {
+  id: 1,
+  firstName: 'Daniel',
+  lastName: 'Soheil',
+  role: { id: 1 },
+  nif: 252021,
+  email: 'daniel@sohe.ir',
+  password: 'hackme',
+  position: 'Developer',
+  isActive: true,
+  createdAt: 'YYYY-MM-DD HH-mm-ss',
+  updatedAt: 'YYYY-MM-DD HH-mm-ss',
+  deletedAt: null,
+};
+
+export const InputUserRemove: string[] = [
+  'id',
+  'createdAt',
+  'updatedAt',
+  'deletedAt',
+];
+
+export const InputUserSchema = removeItemsIn(InputUserRemove, UserSchema);
+export type InputUserType = Static<typeof InputUserSchema>;
+export const InputUserExample: InputUserType = removeItemsIn(
+  InputUserRemove,
+  UserExample,
+);
+
+export const OutputUserRemove: string[] = [
+  'password',
+  'deletedAt',
+];
+
+export const OutputUserSchema = removeItemsIn(OutputUserRemove, UserSchema);
+export type OutputUserType = Static<typeof OutputUserSchema>;
+export const OutputUserExample: OutputUserType = removeItemsIn(
+  OutputUserRemove,
+  UserExample,
+);
