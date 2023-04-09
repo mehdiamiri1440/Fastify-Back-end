c61ce08370d79916209c361049b08672253f661d
Daniel <daniel@sohe.ir>
2023-04-09 18:06:57 +0330

    chore(user): validate role of user in create/update of user

--- src/domains/user/routes/users.ts
+++ src/domains/user/routes/users.ts
@@ -2,6 +2,7 @@ import { ResponseShape } from '$src/infra/Response';
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { usersAuth } from '$src/authentication/users';
 import { User } from '../../user/models/User';
+import { Role } from '../../user/models/Role';
 import { repo } from '$src/databases/typeorm';
 import { ListQueryOptions } from '$src/infra/tables/schema_builder';
 import { TableQueryBuilder } from '$src/infra/tables/Table';
@@ -10,6 +11,7 @@ import {
   InputUserType,
 } from '$src/domains/user/schemas/user.schema';
 const Users = repo(User);
+const Roles = repo(Role);
 import { Type } from '@sinclair/typebox';
 
 const plugin: FastifyPluginAsyncTypebox = async function (app) {
@@ -49,6 +51,10 @@ const plugin: FastifyPluginAsyncTypebox = async function (app) {
       body: InputUserSchema,
     },
     async handler(req) {
+      // validating role
+      const role_ = await Roles.findOneBy({ id: req.body.role });
+      if (!role_) return 'error';
+
       return await Users.save({
         ...(req.body as InputUserType),
         creator: { id: req.user.id },
@@ -72,6 +78,10 @@ const plugin: FastifyPluginAsyncTypebox = async function (app) {
       }),
     },
     async handler(req) {
+      // validating role
+      const role_ = await Roles.findOneBy({ id: req.body.role });
+      if (!role_) return 'error';
+
       return await Users.update(
         { id: req.params.id },
         req.body as InputUserType,
