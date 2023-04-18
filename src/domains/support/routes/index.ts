9d5d785a54b2aab1f8b645f2aa9cbadcc6e42994
Daniel <daniel@sohe.ir>
2023-04-18 09:50:32 +0330

    refactor(support): removed imports not used

--- src/domains/support/routes/index.ts
+++ src/domains/support/routes/index.ts
@@ -1,9 +1,5 @@
 import { ResponseShape } from '$src/infra/Response';
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { Message } from '../models/Message';
-import { repo } from '$src/databases/typeorm';
-
-const Messages = repo(Message);
 
 const plugin: FastifyPluginAsyncTypebox = async function (app) {
   app.register(ResponseShape);
