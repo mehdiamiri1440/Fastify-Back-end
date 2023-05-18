8d80561f2c83072b7ff5ecc3c2dbf797b46570be
Erfan Safari <erfanshield@outlook.com>
2023-05-18 05:46:12 +0000

    Erfanium/config catergories

--- src/domains/configuration/schemas/category.schema.ts
+++ src/domains/configuration/schemas/category.schema.ts
@@ -3,6 +3,7 @@ import { Type } from '@sinclair/typebox';
 export const CategorySchema = Type.Object({
   id: Type.Number(),
   name: Type.String({ minLength: 1 }),
+  parentId: Type.Union([Type.Null(), Type.Integer()]),
   creator: Type.Number(),
   createdAt: Type.Union([Type.Date(), Type.String({ format: 'date-time' })]),
   updatedAt: Type.Union([Type.Date(), Type.String({ format: 'date-time' })]),
