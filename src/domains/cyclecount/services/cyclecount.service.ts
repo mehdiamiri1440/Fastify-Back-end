6a203240c8648c3f17242e27e1f033994cb86642
Daniel <daniel@sohe.ir>
2023-05-29 17:24:03 +0330

    refactor(cyclecount): we dont need this

--- src/domains/cyclecount/services/cyclecount.service.ts
+++ src/domains/cyclecount/services/cyclecount.service.ts
@@ -21,16 +21,6 @@ const CYCLE_COUNT_IS_NOT_OPEN = createError(
   'this cycle count is not open',
   400,
 );
-const NOT_CHECKED = createError(
-  'NOT_CHECKED',
-  'difference not checked by checker in last 5 days',
-  400,
-);
-const NOT_COUNTED = createError(
-  'NOT_COUNTED',
-  'difference not counted by counter',
-  400,
-);
 const MISS_PRODUCT = createError(
   'MISS_PRODUCT',
   'miss product id in cycle count type Product',
