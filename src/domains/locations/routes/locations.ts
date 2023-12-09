4b944a488411724bbe71d9e76bfa046e30976b10
mehdiamiri1440 <knownmma@gmail.com>
2023-12-08 22:55:07 -0800

    done

--- src/domains/locations/routes/locations.ts
+++ src/domains/locations/routes/locations.ts
@@ -1,43 +1,43 @@
-import { UserSchema } from '$src/domains/user/schemas/user.schema';
-import { ResponseShape } from '$src/infra/Response';
-import {
-  Filter,
-  OrderBy,
-  PaginatedQueryString,
-  Searchable,
-} from '$src/infra/tables/PaginatedType';
-import { TableQueryBuilder } from '$src/infra/tables/Table';
-import { repo } from '$src/infra/utils/repo';
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { Type } from '@sinclair/typebox';
-import bcrypt from 'bcrypt';
-import { Location } from '../models/Location';
-import { Like } from 'typeorm';
+// import { UserSchema } from '$src/domains/user/schemas/user.schema';
+// import { ResponseShape } from '$src/infra/Response';
+// import {
+//   Filter,
+//   OrderBy,
+//   PaginatedQueryString,
+//   Searchable,
+// } from '$src/infra/tables/PaginatedType';
+// import { TableQueryBuilder } from '$src/infra/tables/Table';
+// import { repo } from '$src/infra/utils/repo';
+// import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+// import { Type } from '@sinclair/typebox';
+// import bcrypt from 'bcrypt';
+// import { Location } from '../models/Location';
+// import { Like } from 'typeorm';
 
-const plugin: FastifyPluginAsyncTypebox = async function (app) {
-  const Locations = repo(Location);
-  app.register(ResponseShape);
+// const plugin: FastifyPluginAsyncTypebox = async function (app) {
+//   const Locations = repo(Location);
+//   app.register(ResponseShape);
 
-  app.route({
-    method: 'GET',
-    url: '/:zipCode',
-    schema: {
-      params: Type.Object({
-        zipCode: Type.Integer(),
-      }),
-      querystring: PaginatedQueryString({
-        orderBy: OrderBy(['id', 'createdAt']),
-      }),
-    },
-    async handler(req) {
-      const { zipCode } = req.params as { zipCode?: number };
-      return new TableQueryBuilder(Locations, req)
-        .where({
-          zipCode: zipCode ? Like(`%${zipCode}%`) : undefined,
-        })
-        .exec();
-    },
-  });
-};
+//   app.route({
+//     method: 'GET',
+//     url: '/:zipCode',
+//     schema: {
+//       params: Type.Object({
+//         zipCode: Type.Integer(),
+//       }),
+//       querystring: PaginatedQueryString({
+//         orderBy: OrderBy(['id', 'createdAt']),
+//       }),
+//     },
+//     async handler(req) {
+//       const { zipCode } = req.params as { zipCode?: number };
+//       return new TableQueryBuilder(Locations, req)
+//         .where({
+//           zipCode: zipCode ? Like(`%${zipCode}%`) : undefined,
+//         })
+//         .exec();
+//     },
+//   });
+// };
 
-export default plugin;
+// export default plugin;
